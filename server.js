7d330f9b3858ddd3174301dfcbdd7670a301a2d6
Jacoby <mackeyjacoby@gmail.com>
2023-09-23 16:45:18 -0400

    updated CRUD

--- server.js
+++ server.js
@@ -20,8 +20,64 @@ db.on('error', (err) => { console.log('ERROR: ' , err)})
 db.on('connected', () => { console.log('mongo connected')})
 db.on('disconnected', () => { console.log('mongo disconnected')})
 
+// Define a schema for your to-do items
+const todoSchema = new mongoose.Schema({
+    title: String,
+    completed: Boolean
+})
+
+// Create a model based on the schema
+const Todo = mongoose.model('Todo', todoSchema)
+
+// Middleware for parsing JSON request bodies
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
    res.send('Hello!')
+})
+
+// Route to create a new to-do item
+app.post('/todos', async (req, res) => {
+    try {
+        const { title, completed } = req.body
+        const todo = new Todo({ title, completed })
+        await todo.save()
+        res.status(201).json(todo)
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+// Route to update a to-do item by ID
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { title, completed } = req.body
+        const todo = await Todo.findByIdAndUpdate(id, { title, completed }, { new: true })
+        if (!todo) {
+            res.status(404).json({ error: 'Todo not found' })
+        } else {
+            res.status(200).json(todo)
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+
+// Route to delete a to-do item by ID
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const todo = await Todo.findByIdAndRemove(id)
+        if (!todo) {
+            res.status(404).json({ error: 'Todo not found' })
+        } else {
+            res.status(204).send()
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
 
 app.listen(PORT, () => {
