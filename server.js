39f53d9307f2f95d92e68d73f72975ce41930774
Jacoby <mackeyjacoby@gmail.com>
2023-09-24 21:57:51 -0400

    updated

--- server.js
+++ server.js
@@ -1,43 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
 
-// IMPORTS
-const express = require('express')
-//const methodOverride = require('method-override')
-const app = express()
+const app = express();
 
-//app.set("view engine, "ejs")
+// conenction to mongodb
+mongoose.connect("mongodb://localhost/todo_express", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
-
-require('dotenv').config()
-
-const PORT = process.env.PORT || 3000
-
-// setup database 
-const mongoose = require('mongoose')
-const mongoURI = process.env.MONGO_URI
-
-// connect to mongo 
-mongoose.connect(mongoURI)
-
-const db = mongoose.connection
-// optional create status messages to check mongo connection 
-db.on('error', (err) => { console.log('ERROR: ' , err)})
-db.on('connected', () => { console.log('mongo connected')})
-db.on('disconnected', () => { console.log('mongo disconnected')})
-
-//Express Route
-app.get('/', (req, res) => {
-   res.send('Ho world!')
-})
-
-//catch all routes
-app.get("*", function(req, res) {
-    res.send("<h1>Invalid Page</h1>")
-}) 
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`)
-})
+// routes
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
 
+
+// server configurations....
+app.listen(3000, () => console.log("Server started listening on port: 3000"));
