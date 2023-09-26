c8331b4da75915ea20bb18347131800c42f97d8c
Jacoby <mackeyjacoby@gmail.com>
2023-09-26 15:18:48 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,42 +1,110 @@
-const express = require('express');
-const app = express();
-const router = express.Router(); // Define the router
 
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+// // IMPORTS
+// const express = require('express')
+// const app = express()
+// const todoRouter = require('./routes/todo')
+
+
+
+// require('dotenv').config()
+// const PORT = process.env.PORT || 3000
+
+// // setup database 
+// const mongoURI = process.env.MONGO_URI
+// const mongoose = require('mongoose')
+
+
+// // connect to mongo 
+// mongoose.connect(mongoURI)
+
+// // middlewares
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', todoRouter)
+
+// app.set("view engine", "ejs");
+
+// // routes
+// app.use(require("./routes/index"))
+
+// const db = mongoose.connection
+// // optional create status messages to check mongo connection 
+// db.on('error', (err) => { console.log('ERROR: ' , err)})
+// db.on('connected', () => { console.log('mongo connected')})
+// db.on('disconnected', () => { console.log('mongo disconnected')})
+
+// //Express Route
+// // app.get('/', (req, res) => {
+// //    res.send('Ho world!')
+// // })
+
+// //catch all routes
+//  app.get("*", function(req, res) {
+//     res.send("<h1>Invalid Page</h1>")
+//  }) 
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on PORT: ${PORT}`)
+// })
+
+
+const express = require('express')
+const app = express()
+const todoRouter = require('./routes/todo')
+
+// conenction to mongodb
+// mongoose.connect("mongodb://localhost/todo_express", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
 
 // setup database 
-const mongoURI = process.env.MONGO_URI;
-const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI
+const mongoose = require('mongoose')
+
 
 // connect to mongo 
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI)
 
-// routes
-app.use('/', router); // Use the router
-
-// Middleware should come after defining the router
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//app.use(express.static("public"));
+//app.set("view engine", "ejs");
+
+app.use('/', todoRouter)
 
 app.set("view engine", "ejs");
 
-app.use(require("./routes/index"));
+// routes
+app.use(require("./routes/index"))
+//app.use(require("./routes/todo"))
 
-const db = mongoose.connection;
+const db = mongoose.connection
 // optional create status messages to check mongo connection 
-db.on('error', (err) => { console.log('ERROR: ', err) });
-db.on('connected', () => { console.log('mongo connected') });
-db.on('disconnected', () => { console.log('mongo disconnected') });
+db.on('error', (err) => { console.log('ERROR: ' , err)})
+db.on('connected', () => { console.log('mongo connected')})
+db.on('disconnected', () => { console.log('mongo disconnected')})
 
-// catch all routes
-app.get("*", function (req, res) {
-  res.send("<h1>Invalid Page</h1>");
-});
+//Express Route
+// app.get('/', (req, res) => {
+//    res.send('Ho world!')
+// })
+
+//catch all routes
+ app.get("*", function(req, res) {
+    res.send("<h1>Invalid Page</h1>")
+ }) 
+
+
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`);
-});
-
-module.exports = router; // Export the router
-
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
