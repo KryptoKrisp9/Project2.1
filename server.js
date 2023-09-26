6790140a1803d492556abb3409bb8f72f63a13b8
Jacoby <mackeyjacoby@gmail.com>
2023-09-26 10:16:25 -0400

    heroku logs

--- server.js
+++ server.js
@@ -1,52 +1,42 @@
+const express = require('express');
+const app = express();
+const router = express.Router(); // Define the router
 
-// IMPORTS
-const express = require('express')
-const app = express()
-const todoRouter = require('./routes/todo')
-
-
-
-require('dotenv').config()
-const PORT = process.env.PORT || 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 // setup database 
-const mongoURI = process.env.MONGO_URI
-const mongoose = require('mongoose')
-
+const mongoURI = process.env.MONGO_URI;
+const mongoose = require('mongoose');
 
 // connect to mongo 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI);
 
-// middlewares
+// routes
+app.use('/', router); // Use the router
+
+// Middleware should come after defining the router
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', todoRouter)
-
 app.set("view engine", "ejs");
 
-// routes
-app.use(require("./routes/index"))
+app.use(require("./routes/index"));
 
-const db = mongoose.connection
+const db = mongoose.connection;
 // optional create status messages to check mongo connection 
-db.on('error', (err) => { console.log('ERROR: ' , err)})
-db.on('connected', () => { console.log('mongo connected')})
-db.on('disconnected', () => { console.log('mongo disconnected')})
+db.on('error', (err) => { console.log('ERROR: ', err) });
+db.on('connected', () => { console.log('mongo connected') });
+db.on('disconnected', () => { console.log('mongo disconnected') });
 
-//Express Route
-// app.get('/', (req, res) => {
-//    res.send('Ho world!')
-// })
-
-//catch all routes
- app.get("*", function(req, res) {
-    res.send("<h1>Invalid Page</h1>")
- }) 
-
-
+// catch all routes
+app.get("*", function (req, res) {
+  res.send("<h1>Invalid Page</h1>");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`)
-})
+  console.log(`Server is listening on PORT: ${PORT}`);
+});
+
+module.exports = router; // Export the router
 
